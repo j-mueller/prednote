d554688d01b9f9136cb29f537c7cdcd7d5c3699a
Omari Norman <omari@smileystation.com>
2013-03-22 16:24:04 -0400

    add docs

--- Data/Prednote/TestTree.hs
+++ Data/Prednote/TestTree.hs
@@ -16,6 +16,9 @@
 -- module comes in.
 --
 -- > {-# LANGUAGE OverloadedStrings #-}
+-- > import System.Console.Rainbow
+-- > import Data.Prednote.TestTree
+-- > import Data.Prednote.Pdct
 -- >
 -- > isOdd :: Pdct Int
 -- > isOdd = operand "is odd" odd
@@ -26,8 +29,32 @@
 -- > greaterThan20 :: Pdct Int
 -- > greaterThan20 = operand "greater than 20" (> 20)
 -- >
+-- > myOpts :: TestOpts Int
+-- > myOpts = TestOpts
+-- >   { tIndentAmt = 2
+-- >   , tPassVerbosity = TrueSubjects
+-- >   , tFailVerbosity = TrueSubjects
+-- >   , tGroupPred = const True
+-- >   , tTestPred = const True
+-- >   , tShowSkippedTests = True
+-- >   , tGroupVerbosity = AllGroups
+-- >   , tSubjects = mySubjects
+-- >   , tStopOnFail = False
+-- >   }
+-- >
+-- > mySubjects :: [Int]
+-- > mySubjects = [2, 4, 6, 8, 10, 18, 19, 20, 21, 22, 24, 26]
+-- >
 -- > tests :: [TestTree Int]
--- > tests = undefined
+-- > tests = [ isOdd, greaterThan0, greaterThan20 ]
+-- >
+-- > main :: IO ()
+-- > main = do
+-- >   let (cks, passed, failed) = runTests myOpts 0 tests
+-- >   t <- termFromEnv
+-- >   printChunks t cks
+-- >   putStrLn $ "number of tests passed: " ++ show passed
+-- >   putStrLn $ "number of tests failed: " ++ show failed
 module Data.Prednote.TestTree
   (
   -- * The TestTree
@@ -47,19 +74,16 @@ module Data.Prednote.TestTree
   -- * Simple test runners
   , Verbosity(..)
   , GroupVerbosity (..)
-  , TestOpts (..)
   , Pt.Level
   , PassCount
   , FailCount
-  , runAllTests
-  , AllPassed
-  , runUntilFailure
+  , runTests
 
   -- * Showing the test tree
   , showTestTree
 
   -- * Tree evaluator
-  , EvalEnv (..)
+  , TestOpts (..)
   , ShortCircuit
   , Pass
   , evalTree
@@ -83,6 +107,8 @@ import qualified Data.Prednote.Pdct as Pt
 --
 
 type Pass = Bool
+
+-- | The name of a test or of a group.
 type Name = Text
 
 -- | A tree of tests.
@@ -92,6 +118,8 @@ data Payload a
   = Group [TestTree a]
   | Test (TestFunc a)
 
+-- | A test is a function of this type. The function must make chunks
+-- in a manner that respects the applicable verbosity.
 type TestFunc a
   = Pt.IndentAmt
   -> Verbosity
@@ -105,13 +133,20 @@ type TestFunc a
   -> (Pass, [R.Chunk])
 
 
+-- | Creates groups of tests.
 group :: Name -> [TestTree a] -> TestTree a
-group n ts = TestTree n (Group ts)
+group n = TestTree n . Group
 
+-- | Creates tests.
 test :: Name -> TestFunc a -> TestTree a
-test n t = TestTree n (Test t)
+test n = TestTree n . Test
 
+-- | How verbose to be when reporting the results of tests. It would
+-- be possible to have many more knobs to control this behavior; this
+-- implementation is a compromise and hopefully provides enough
+-- verbosity settings without being too complex.
 data Verbosity
+
   = Silent
   -- ^ Show nothing at all
 
@@ -119,16 +154,21 @@ data Verbosity
   -- ^ Show only whether the test passed or failed
 
   | FalseSubjects
-  -- ^ Show subjects that are False
+  -- ^ Show subjects that are False. In addition, shows all evaluation
+  -- steps that led to the subject being False; however, does not show
+  -- discarded evaluation steps. Does not show True subjects at all.
 
   | TrueSubjects
   -- ^ Show subjects that are True. (This is cumulative, so False
-  -- subjects are shown too.)
+  -- subjects are shown too, as they would be using 'FalseSubjects'.)
+  -- Shows all evaluation steps that led to the subject being True;
+  -- however, does not show discarded evaluation steps.
 
-  | DiscardedSubjects
+  | Discards
+  -- ^ Shows discarded subjects. Cumulative, so also does what
+  -- 'FalseSubjects' and 'TrueSubjects' do. Also shows all discarded
+  -- evaluation steps for all subjects.
 
-  | DiscardedPredicates
-  -- ^ Show discarded results
   deriving (Eq, Ord, Show)
 
 --
@@ -163,8 +203,7 @@ isSubjectAndDiscardsShown v b = case v of
   PassFail -> (False, False)
   FalseSubjects -> (not . isTrue $ b, False)
   TrueSubjects -> (isJust b, False)
-  DiscardedSubjects -> (True, False)
-  DiscardedPredicates -> (True, True)
+  Discards -> (True, True)
 
 
 showTestTitle :: Pt.IndentAmt -> Pt.Level -> Name -> Pass -> [R.Chunk]
@@ -259,64 +298,108 @@ showTestTree amt l (TestTree n p) = indent amt l n : children
       Test _ -> []
 
 
-data EvalEnv a = EvalEnv
-  { eeIndentAmt :: Int
-  , eePassVerbosity :: Verbosity
-  , eeFailVerbosity :: Verbosity
-  , eeShowSkippedTests :: Bool
-  , eeGroupVerbosity :: GroupVerbosity
-  , eeGroupPred :: Name -> Bool
-  , eeTestPred :: Name -> Bool
-  , eeSubjects :: [a]
-  , eeStopOnFail :: Bool
+-- | Options for running tests.
+data TestOpts a = TestOpts
+
+  { tIndentAmt :: Int
+    -- ^ Indent each level by this many spaces
+
+  , tPassVerbosity :: Verbosity
+    -- ^ Use this verbosity for tests that pass
+
+  , tFailVerbosity :: Verbosity
+    -- ^ Use this verbosity for tests that fail
+
+  , tGroupPred :: Name -> Bool
+    -- ^ Groups are run only if this predicate returns True.
+
+  , tTestPred :: Name -> Bool
+    -- ^ Tests are run only if this predicate returns True.
+
+  , tShowSkippedTests :: Bool
+    -- ^ Some tests might be skipped; see 'tGroupPred' and
+    -- 'tTestPred'. This controls whether you want to see a
+    -- notification of tests that were skipped.
+
+  , tGroupVerbosity :: GroupVerbosity
+    -- ^ Show group names? Even if you do not show the names of
+    -- groups, tests within the group will still be indented.
+
+  , tSubjects :: [a]
+    -- ^ The subjects to test
+
+  , tStopOnFail :: Bool
+    -- ^ If True, then tests will stop running immediately after a
+    -- single test fails. If False, all tests are always run.
   }
 
 
+-- | True if the tree returned a result without completely evaluating
+-- all parts of the tree. This can occur if 'tStopOnFail' is True and
+-- one of the tests in the tree failed.
 type ShortCircuit = Bool
 
+-- | Evaluates a tree. This function is the basis of 'runTests', which
+-- is typically a bit easier to use.
 evalTree
-  :: EvalEnv a
+
+  :: TestOpts a
+  -- ^ Most options
+
   -> Pt.Level
+  -- ^ The tree will indented by this many levels; typically you will
+  -- want to start this at 0.
+
   -> TestTree a
+
   -> (ShortCircuit, [Either [R.Chunk] (Pass, [R.Chunk])])
+  -- ^ The first element of the tuple is True if the tree was not
+  -- fully evaluated. This can happen if 'tStopOnFail' is True and
+  -- one of the tests in the tree failed. The second element of the
+  -- tuple is a list of Either; each element of the list will be a
+  -- Left if that component of the tree was not a test, or a Right if
+  -- that element was a test. The Right will contain a tuple, where
+  -- the first element indicates whether the test passed or failed,
+  -- and the second element is the list of Chunk.
+
 evalTree ee l (TestTree n p) = case p of
   Group ts -> evalGroup ee n l ts
   Test f -> evalTest ee n l f
 
 evalGroup
-  :: EvalEnv a
+  :: TestOpts a
   -> Name
   -> Pt.Level
   -> [TestTree a]
   -> (ShortCircuit, [Either [R.Chunk] (Pass, [R.Chunk])])
-evalGroup ee n l ts = if eeGroupPred ee n
+evalGroup ee n l ts = if tGroupPred ee n
   then let ls = unfoldr (unfoldList ee l) (False, ts)
            stop = any not . map fst $ ls
            rslts = concat . map snd $ ls
-           groupNm = if eeGroupVerbosity ee /= NoGroups
-                     then indent (eeIndentAmt ee) l n
+           groupNm = if tGroupVerbosity ee /= NoGroups
+                     then indent (tIndentAmt ee) l n
                      else R.plain ""
         in (stop, Left [groupNm] : rslts)
-  else let groupNm = if eeGroupVerbosity ee == AllGroups
-                     then skip "group" (eeIndentAmt ee) l n
+  else let groupNm = if tGroupVerbosity ee == AllGroups
+                     then skip "group" (tIndentAmt ee) l n
                      else [R.plain ""]
        in (False, [Left groupNm])
 
 
 evalTest
-  :: EvalEnv a
+  :: TestOpts a
   -> Name
   -> Pt.Level
   -> TestFunc a
   -> (ShortCircuit, [Either [R.Chunk] (Pass, [R.Chunk])])
-evalTest ee n l tf = if eeTestPred ee n
+evalTest ee n l tf = if tTestPred ee n
   then (not p, [Right (p, cs)])
   else (False, skipped)
   where
-    (p, cs) = tf (eeIndentAmt ee) (eePassVerbosity ee)
-              (eeFailVerbosity ee) (eeSubjects ee) l
-    skipped = if eeShowSkippedTests ee
-              then [Left $ skip "test" (eeIndentAmt ee) l n]
+    (p, cs) = tf (tIndentAmt ee) (tPassVerbosity ee)
+              (tFailVerbosity ee) (tSubjects ee) l
+    skipped = if tShowSkippedTests ee
+              then [Left $ skip "test" (tIndentAmt ee) l n]
               else []
 
 
@@ -327,6 +410,7 @@ evalTest ee n l tf = if eeTestPred ee n
 type PassCount = Int
 type FailCount = Int
 
+-- | How verbose to be when showing names of groups.
 data GroupVerbosity
 
   = NoGroups
@@ -340,58 +424,32 @@ data GroupVerbosity
   -- ^ Show all groups, and indicate which groups are skipped.
   deriving (Eq, Ord, Show)
 
-data TestOpts a = TestOpts
-  { tIndentAmt :: Int
-  , tPassVerbosity :: Verbosity
-  , tFailVerbosity :: Verbosity
-  , tShowSkippedTests :: Bool
-  , tGroupVerbosity :: GroupVerbosity
-  , tGroupPred :: Name -> Bool
-  , tTestPred :: Name -> Bool
-  , tSubjects :: [a]
-  }
 
--- | Runs all tests. Reports on how many passed and how many failed.
-
-runAllTests
+-- | Runs each test in a list of tests (though each test might not run
+-- if 'tStopOnFail' is True.) Reports on how many passed and how many
+-- failed. (if 'tStopOnFail' is True, the FailCount will never exceed
+-- 1.)
+runTests
   :: TestOpts a
   -> Pt.Level
   -> [TestTree a]
   -> ([R.Chunk], PassCount, FailCount)
-runAllTests (TestOpts i pv fv sk gv gp tp ss) l ts =
-  let ee = EvalEnv i pv fv sk gv gp tp ss False
-      rs = concatMap snd . map (evalTree ee l) $ ts
-      testRs = rights rs
+runTests ee l ts =
+  let ls = unfoldr (unfoldList ee l) (False, ts)
+      testRs = rights . concatMap snd $ ls
       passed = length . filter id . map fst $ testRs
       failed = length . filter (not . id) . map fst $ testRs
-      cks = concat . map (either id snd) $ rs
+      cks = concat . map (either id snd) . concatMap snd $ ls
   in (cks, passed, failed)
 
-
-type AllPassed = Bool
-
--- | Runs tests. Stops running tests if a single test fails. Returns
--- True if all tests passed, or False if a single test failed.
-runUntilFailure
-  :: TestOpts a
-  -> Pt.Level
-  -> [TestTree a]
-  -> ([R.Chunk], AllPassed)
-runUntilFailure (TestOpts i pv fv sk gv gp tp ss) l ts =
-  let ee = EvalEnv i pv fv sk gv gp tp ss True
-      ls = unfoldr (unfoldList ee l) (False, ts)
-      allPass = and . map not . map fst $ ls
-      cks = concat . map (either id snd) . concatMap snd $ ls
-  in (cks, allPass)
-
 unfoldList
-  :: EvalEnv a
+  :: TestOpts a
   -> Pt.Level
   -> (ShortCircuit, [TestTree a])
   -> Maybe ( (ShortCircuit, [Either [R.Chunk] (Pass, [R.Chunk])])
            , (ShortCircuit, [TestTree a]))
 unfoldList ee l (seenFalse, is) =
-  if seenFalse && eeStopOnFail ee
+  if seenFalse && tStopOnFail ee
   then Nothing
   else case is of
         [] -> Nothing
