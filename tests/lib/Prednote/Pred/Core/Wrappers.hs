73a6a26dea4bd4d778f05e441d11a25bce68ba2a
Omari Norman <omari@smileystation.com>
2014-06-28 17:06:52 -0400

    WIP

--- tests/lib/Prednote/Pred/Core/Wrappers.hs
+++ tests/lib/Prednote/Pred/Core/Wrappers.hs
@@ -7,6 +7,7 @@ import Control.Monad
 import System.Console.Rainbow.Wrappers
 import qualified System.Console.Rainbow as R
 import Test.SmallCheck.Series
+import Control.Arrow
 
 newtype Output = Output { unOutput :: C.Output }
   deriving Show
@@ -14,6 +15,17 @@ newtype Output = Output { unOutput :: C.Output }
 instance Arbitrary Output where
   arbitrary = fmap Output $ liftM3 C.Output arbitrary arbitrary
     intToChunk
+
+instance Monad m => Serial m Output where
+  series = fmap Output $ cons3 f
+    where
+      f b res rpt = C.Output b res (fmap (map unChunk) rpt)
+
+instance Monad m => CoSerial m Output where
+  coseries rs =
+    alts3 rs >>- \f3 ->
+    return $ \(Output (C.Output b res rpt)) ->
+    f3 b res (fmap (map Chunk) rpt)
 
 intToChunk :: Gen (Int -> [R.Chunk])
 intToChunk = fmap (fmap (map unChunk)) arbitrary
@@ -28,8 +40,52 @@ instance Arbitrary Shortable where
         (4, fmap Just $ liftM2 (,) arbitrary intToChunk)])
       (fmap unOutput arbitrary)
 
+instance Monad m => Serial m Shortable where
+  series = fmap Shortable $ cons2 f
+    where
+      f mayShort out = C.Shortable shrt (unOutput out)
+        where
+          shrt = case mayShort of
+            Nothing -> Nothing
+            Just (i, fn) -> Just (i, fmap (map unChunk) fn)
+
+instance Monad m => CoSerial m Shortable where
+  coseries rs =
+    alts2 rs >>- \f2 ->
+    return $ \(Shortable (C.Shortable shrt out)) ->
+    f2 (fmap (second (fmap (map Chunk))) shrt) (Output out)
+
 newtype Calc = Calc { unCalc :: C.Calc Int }
   deriving Show
+
+expand :: Int -> [Int]
+expand i = everyOther [negate (abs i) .. abs i]
+  where
+    everyOther ls = case ls of
+      [] -> []
+      x:[] -> [x]
+      x:_:xs -> x : everyOther xs
+
+instance Monad m => Serial m Calc where
+  series = fmap Calc $
+    cons1 (\pd -> C.Predicate (fmap unOutput pd))
+    \/ cons2 (\(Pred pd) f -> C.Single pd (fmap (fmap unOutput) f))
+    \/ cons2 (\ls f -> C.Variable (map unPred ls)
+                       (fmap (fmap unShortable) f))
+    \/ cons3 (\g pd f -> C.Fan g (unPred pd)
+                         (fmap (fmap unShortable) f))
+
+instance Monad m => CoSerial m Calc where
+  coseries rs =
+    alts1 rs >>- \f1 ->
+    alts2 rs >>- \f2a ->
+    alts2 rs >>- \f2b ->
+    alts3 rs >>- \f3 ->
+    return $ \(Calc c) -> case c of
+      C.Predicate p -> f1 (fmap Output p)
+      C.Single p o -> f2a (Pred p) (fmap (fmap Output) o)
+      C.Variable ps o -> f2b (map Pred ps) (fmap (fmap Shortable) o)
+      C.Fan g pd f -> f3 g (Pred pd) (fmap (fmap Shortable) f)
 
 instance Arbitrary Calc where
   arbitrary = sized $ \s -> fmap Calc $ oneof
@@ -48,6 +104,12 @@ instance Arbitrary Calc where
 
 newtype Pred = Pred { unPred :: C.Pred Int }
   deriving Show
+
+instance Monad m => Serial m Pred where
+  series = undefined
+
+instance Monad m => CoSerial m Pred where
+  coseries = undefined
 
 instance Arbitrary Pred where
   arbitrary = fmap Pred $
