8944eb08910b15632588bff410e7c57ef035da1c
Omari Norman <omari@smileystation.com>
2014-06-29 10:48:49 -0400

    WIP

--- library/lib/Prednote/Pred.hs
+++ library/lib/Prednote/Pred.hs
@@ -5,6 +5,99 @@
 -- over the 'Pred' produced, use "Prednote.Pred.Core".
 --
 -- Exports some names that conflict with Prelude names, such as 'and',
--- 'or', 'not', and 'filter'; keep this in mind when you do your
--- @import@.
+-- 'or', 'not', and 'filter'; keep this in mind when you
+-- @import@ this module.
 module Prednote.Pred where
+
+import qualified Prednote.Pred.Core as C
+import Prednote.Pred.Core (Pred)
+import qualified Data.Text as X
+import Data.Text (Text)
+import System.Console.Rainbow
+import Data.Monoid
+import Prelude hiding (and, or, not, filter)
+
+lblTrue :: [Chunk]
+lblTrue = ["[", f_green <> "TRUE", "]"]
+
+lblFalse :: [Chunk]
+lblFalse = ["[", f_red <> "FALSE", "]"]
+
+indentAmt :: Int
+indentAmt = 2
+
+lblLine :: Bool -> Text -> [Chunk]
+lblLine b t = lbl ++ [" ", fromText t]
+  where
+    lbl | b = lblTrue
+        | otherwise = lblFalse
+
+indent :: [Chunk] -> Int -> [Chunk]
+indent cs i = spaces : cs ++ [fromText "\n"]
+  where
+    spaces = fromText . X.replicate (indentAmt * i)
+      . X.singleton $ ' '
+
+shortCir :: Int -> [Chunk]
+shortCir = indent ["[", f_yellow <> "short circuit" <> "]"]
+
+indentTxt :: Text -> Int -> [Chunk]
+indentTxt = indent . (:[]) . fromText
+
+predicate
+  :: Text
+  -- ^ Static name
+  -> (a -> (Bool, Bool, Text))
+  -- ^ Compute result.  The first element of the result tuple is the
+  -- result itself.  The second is whether the result is visible, and
+  -- the third is a label.
+  -> Pred a
+predicate l f = C.predicate (indentTxt l) f'
+  where
+    f' a = (r, v, indentTxt dyn)
+      where
+        (r, v, dyn) = f a
+
+and :: [Pred a] -> Pred a
+and = C.and (indentTxt "and") shortCir dyn
+  where
+    dyn b _ = indent $ lblLine b "and"
+
+(&&&) :: Pred a -> Pred a -> Pred a
+l &&& r = and [l, r]
+
+infixr 3 &&&
+
+or :: [Pred a] -> Pred a
+or = C.or (indentTxt "or") shortCir dyn
+  where
+    dyn b _ = indent $ lblLine b "or"
+
+(|||) :: Pred a -> Pred a -> Pred a
+l ||| r = or [l, r]
+
+infixr 2 |||
+
+not :: Pred a -> Pred a
+not = C.not (indentTxt "not") dyn
+  where
+    dyn b _ = indent $ lblLine b "not"
+
+fanand :: (a -> [b]) -> Pred b -> Pred a
+fanand = C.fanand (indentTxt lbl) shortCir dyn
+  where
+    lbl = "fanout and - no fanned-out subject may be False"
+    dyn b _ = indent $ lblLine b lbl
+
+fanor :: (a -> [b]) -> Pred b -> Pred a
+fanor = C.fanor (indentTxt lbl) shortCir dyn
+  where
+    lbl = "fanout or - one fanned-out subject must be True"
+    dyn b _ = indent $ lblLine b lbl
+
+fanAtLeast :: Int -> (a -> [b]) -> Pred b -> Pred a
+fanAtLeast i = C.fanAtLeast i (indentTxt lbl) shortCir dyn
+  where
+    lbl = "fanout at least - at least " <> X.pack (show i)
+      <> " fanned-out subject(s) must be True"
+    dyn b _ = indent $ lblLine b lbl
--- library/lib/Prednote/Pred/Core.hs
+++ library/lib/Prednote/Pred/Core.hs
@@ -2,6 +2,11 @@
 -- | 'Pred' core functions.  If your needs are simple, "Prednote.Pred"
 -- is easier to use.  However, the types and functions in this module
 -- give you more control.
+--
+-- Exports some names that conflict with Prelude names, such as 'and',
+-- 'or', 'not', and 'filter'; keep this in mind when you
+-- @import@ this module.
+
 module Prednote.Pred.Core where
 
 import System.Console.Rainbow
@@ -9,6 +14,8 @@ import Prelude hiding (filter, not)
 import qualified Prelude
 import Data.Functor.Contravariant (Contravariant(..))
 import Data.Tree
+import qualified Data.Text as X
+import Data.Maybe
 
 type Chunker = Int -> [Chunk]
 
@@ -17,28 +24,50 @@ data Pred a = Pred
   , eval :: a -> Tree Output
   }
 
-instance Show (Pred a) where
-  show _ = "Predicate"
-
 instance Contravariant Pred where
   contramap f (Pred s e) = Pred s (e . f)
 
 data Output = Output
   { result :: Bool
+  , visible :: Bool
   , short :: Maybe Chunker
   , dynamic :: Chunker
   }
 
 instance Show Output where
-  show (Output r _ _) = "output: " ++ show r
+  show (Output r v _ _) = "output - result: " ++ show r
+    ++ " visible: " ++ show v
 
 
 predicate
   :: Chunker
-  -> (a -> (Bool, Chunker))
+  -- ^ Static name
+
+  -> (a -> (Bool, Bool, Chunker))
+  -- ^ Compute result
+
   -> Pred a
 predicate st f = Pred (Node st [])
-  (\a -> let (r, c) = f a in Node (Output r Nothing c) [])
+  (\a -> let (r, v, c) = f a in Node (Output r v Nothing c) [])
+
+
+visibility :: (Bool -> Bool) -> Pred a -> Pred a
+visibility f (Pred s e) = Pred s e'
+  where
+    e' a = g (e a)
+    g (Node n cs) = Node n { visible = f (result n) } cs
+
+reveal :: Pred a -> Pred a
+reveal = visibility (const True)
+
+hide :: Pred a -> Pred a
+hide = visibility (const False)
+
+showTrue :: Pred a -> Pred a
+showTrue = visibility id
+
+showFalse :: Pred a -> Pred a
+showFalse = visibility Prelude.not
 
 
 and
@@ -55,7 +84,7 @@ and st ss dyn ls = Pred st' ev
     st' = Node st . map static $ ls
     ev a = go [] ls
       where
-        go soFar [] = Node (Output True Nothing (dyn True a))
+        go soFar [] = Node (Output True True Nothing (dyn True a))
           (reverse soFar)
         go soFar (x:xs) =
           let tree = eval x a
@@ -63,7 +92,7 @@ and st ss dyn ls = Pred st' ev
               shrt = case xs of
                 [] -> Nothing
                 _ -> Just ss
-              out = Output r shrt (dyn r a)
+              out = Output r True shrt (dyn r a)
               cs = reverse (tree:soFar)
           in case xs of
               [] -> Node out cs
@@ -85,7 +114,7 @@ or st ss dyn ls = Pred st' ev
     st' = Node st . map static $ ls
     ev a = go [] ls
       where
-        go soFar [] = Node (Output False Nothing (dyn False a))
+        go soFar [] = Node (Output False True Nothing (dyn False a))
           (reverse soFar)
         go soFar (x:xs) =
           let tree = eval x a
@@ -93,7 +122,7 @@ or st ss dyn ls = Pred st' ev
               shrt = case xs of
                 [] -> Nothing
                 _ -> Just ss
-              out = Output r shrt (dyn r a)
+              out = Output r True shrt (dyn r a)
               cs = reverse (tree:soFar)
           in case xs of
               [] -> Node out cs
@@ -113,14 +142,14 @@ not st dyn pd = Pred st' ev
     st' = Node st [static pd]
     ev a = Node nd [c]
       where
-        nd = Output res Nothing (dyn res a)
+        nd = Output res True Nothing (dyn res a)
         (res, c) = (Prelude.not r, t)
           where
             t = eval pd a
             r = result . rootLabel $ t
 
 fan
-  :: ([Bool] -> (Bool, Maybe Int))
+  :: ([Bool] -> (Bool, Bool, Maybe Int))
 
   -> Chunker
   -- ^ Static label
@@ -141,11 +170,11 @@ fan get st ss dyn fn pd = Pred st' ev
     st' = Node st [static pd]
     ev a = Node nd cs
       where
-        nd = Output r shrt (dyn r a)
+        nd = Output r v shrt (dyn r a)
         bs = fn a
         allcs = map (eval pd) bs
         bools = map (result . rootLabel) allcs
-        (r, mayInt) = get bools
+        (r, v, mayInt) = get bools
         cs = case mayInt of
           Nothing -> allcs
           Just i -> take i allcs
@@ -172,9 +201,9 @@ fanand = fan get
     get = go 0
       where
         go !c ls = case ls of
-          [] -> (True, Just c)
+          [] -> (True, True, Just c)
           x:xs
-            | Prelude.not x -> (False, Just (c + 1))
+            | Prelude.not x -> (False, True, Just (c + 1))
             | otherwise -> go (c + 1) xs
 
 fanor
@@ -197,9 +226,9 @@ fanor = fan get
     get = go 0
       where
         go !c ls = case ls of
-          [] -> (False, Just c)
+          [] -> (False, True, Just c)
           x:xs
-            | x -> (True, Just (c + 1))
+            | x -> (True, True, Just (c + 1))
             | otherwise -> go (c + 1) xs
 
 fanAtLeast
@@ -225,14 +254,61 @@ fanAtLeast i = fan get
     get = go 0 0
       where
         go !found !c ls
-          | found >= i = (True, Just c)
+          | found >= i = (True, True, Just c)
           | otherwise = case ls of
-              [] -> (False, Just c)
+              [] -> (False, True, Just c)
               x:xs -> go fnd' (c + 1) xs
                 where
                   fnd' | x = found + 1
                        | otherwise = found
 
+report
+  :: Int
+  -> Tree Output
+  -> [Chunk]
+report l (Node n cs)
+  | Prelude.not . visible $ n = []
+  | otherwise = this ++ concatMap (report (l + 1)) cs ++ shrt
+  where
+    this = dynamic n l
+    shrt = maybe [] ($ l) . short $ n
+
+plan
+  :: Int
+  -> Pred a
+  -> [Chunk]
+plan lvl pd = go lvl (static pd)
+  where
+    go l (Node n cs) = this ++ concatMap (go (l + 1)) cs
+      where
+        this = n l
+
+evaluate :: Pred a -> a -> Tree Output
+evaluate = eval
+
+instance Show (Pred a) where
+  show = X.unpack . X.concat . concat . map text
+    . plan 0
+
+test :: Pred a -> a -> Bool
+test p = result . rootLabel . eval p
+
+testV :: Pred a -> a -> (Bool, [Chunk])
+testV p a = (result . rootLabel $ t, report 0 t)
+  where
+    t = eval p a
+
+filter :: Pred a -> [a] -> [a]
+filter p = Prelude.filter (test p)
+
+filterV :: Pred a -> [a] -> ([a], [Chunk])
+filterV p as = (mapMaybe fltr (zip as rslts), cks)
+  where
+    fltr (a, r)
+      | result . rootLabel $ r = Just a
+      | otherwise = Nothing
+    rslts = map (eval p) as
+    cks = concatMap (report 0) rslts
 
 -- | @shorter x y@ is True if list x is shorter than list y. Lazier
 -- than taking the length of each list and comparing the results.
