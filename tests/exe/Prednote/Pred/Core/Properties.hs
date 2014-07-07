94a8246b8700b5a9192dd37db207fefcf90403a1
Omari Norman <omari@smileystation.com>
2014-07-07 08:10:34 -0400

    WIP

--- tests/exe/Prednote/Pred/Core/Properties.hs
+++ tests/exe/Prednote/Pred/Core/Properties.hs
@@ -1,7 +1,12 @@
+{-# OPTIONS_GHC -fno-warn-missing-signatures #-}
 module Prednote.Pred.Core.Properties where
 
 import Prednote.Pred.Core.Generators
 import qualified Prednote.Pred.Core as C
+import qualified Data.Tree as E
+import Test.QuickCheck
+import Prednote.Tests.Util
+import Prelude hiding (pred)
 
 -- | Shown values are True
 prop_shown :: Bool
@@ -11,4 +16,10 @@ prop_shown = C.unVisible C.shown
 prop_hidden :: Bool
 prop_hidden = not $ C.unVisible C.hidden
 
-
+-- | Visiblity has the expected result
+prop_visibility =
+  forAll (fmap Blind $ function1 coarbitrary visible) $ \(Blind f) ->
+  forAll pred $ \p ->
+  forAll arbitrary $ \i ->
+  let r = E.rootLabel . ($ i) . C.evaluate . C.visibility f $ p
+  in f (C.result r) == C.visible r
