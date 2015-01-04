12ab8386c820dde14f7447426dfbace87a36fd8a
Omari Norman <omari@smileystation.com>
2015-01-04 11:22:03 -0500

    WIP

--- library/lib/Prednote/Core.hs
+++ library/lib/Prednote/Core.hs
@@ -22,41 +22,78 @@ instance Functor Children where
   fmap f (One t) = One (fmap f t)
   fmap f (Two x y) = Two (fmap f x) (fmap f y)
 
+data Pred a = Pred (Tree [Chunk]) (a -> Out)
+
+data Out = Out [Chunk] OutC
+
+data OutC
+
+  = Terminal Bool
+  -- ^ The bottom of a tree.  Produced by 'predicate'.
+
+  | Hollow Out
+  -- ^ The value of this result is the same as its child.  Produced by
+  -- 'wrap' and 'either'.  Has no independent visibility; if this
+  -- result is shown, its child is also shown.  Whether this result is
+  -- shown depends on the parent's visibility
+
+  | Child1 Bool Visible Out
+  -- ^ The value of this result is determined independently.  Produced
+  -- by 'splitAnd', 'splitOr', 'and', 'or', and 'not'.  Children are
+  -- not shown if the visibiliy is 'hidden'.
+
+  | Child2 Bool Visible Out Out
+  -- ^ The value of this result is determined independently.  Produced
+  -- by 'splitAnd', 'splitOr', 'and', and 'or'.
+
 -- | Is this result visible?  If not, 'Prednote.report' will not show it.
 newtype Visible = Visible Bool
   deriving (Eq, Ord, Show)
 
-shown :: Visible
-shown = Visible True
+showChildren :: Visible
+showChildren = Visible True
 
-hidden :: Visible
-hidden = Visible False
+hideChildren :: Visible
+hideChildren = Visible False
 
-data Pred a = Pred (Tree [Chunk]) (a -> Tree Output)
+outResult :: OutC -> Bool
+outResult c = case c of
+  Terminal b -> b
+  Hollow (Out _ c') -> outResult c'
+  Child1 b _ _ -> b
+  Child2 b _ _ _ -> b
+
 
 runPred :: Pred a -> a -> Bool
-runPred (Pred _ f) a = let (Tree (Output r _ _) _) = f a in r
+runPred (Pred _ f) a = let Out _ c = f a in outResult c
+
+-- | Set visibility of children depending on whether the 'Pred' is
+-- 'True' or 'False'.  Does not affect 'Pred' built with 'predicate',
+-- 'wrap', or 'either'.
 
 visibility :: (Bool -> Visible) -> Pred a -> Pred a
 visibility fVis (Pred lbl f) = Pred lbl f'
   where
-    f' a = Tree o' c
+    f' a = Out cks o'
       where
-        o' = Output res vis' cks
-        (Tree (Output res _ cks) c) = f a
-        vis' = fVis res
+        Out cks o = f a
+        o' = case o of
+          Child1 r _ c -> Child1 r (fVis r) c
+          Child2 r _ c1 c2 -> Child2 r (fVis r) c1 c2
+          x -> x
+
 
 showTrue :: Pred a -> Pred a
 showTrue = visibility f
   where
-    f b | b = shown
-        | otherwise = hidden
+    f b | b = showChildren
+        | otherwise = hideChildren
 
 showFalse :: Pred a -> Pred a
 showFalse = visibility f
   where
-    f b | Prelude.not b = shown
-        | otherwise = hidden
+    f b | Prelude.not b = showChildren
+        | otherwise = hideChildren
 
 hideTrue :: Pred a -> Pred a
 hideTrue = showFalse
@@ -64,23 +101,22 @@ hideTrue = showFalse
 hideFalse :: Pred a -> Pred a
 hideFalse = showTrue
 
-
 instance Show (Pred a) where
   show _ = "Pred"
 
+
 test :: Pred a -> a -> Bool
-test (Pred _ f) a = let (Tree (Output res _ _) _) = f a in res
+test (Pred _ f) a = let Out _ c = f a in outResult c
+
 
 instance Contravariant Pred where
   contramap f (Pred t e) = Pred t (e . f)
-
-data Output = Output Bool Visible [Chunk]
-  deriving (Eq, Ord, Show)
 
 data Annotated a = Annotated [Chunk] a
 
 instance Functor Annotated where
   fmap f (Annotated c a) = Annotated c (f a)
+
 
 predicate
   :: [Chunk]
@@ -89,10 +125,9 @@ predicate
   -> Pred a
 predicate lbl f = Pred (Tree lbl Empty) f'
   where
-    f' a = Tree out Empty
+    f' a = Out cks (Terminal r)
       where
-        out = Output res shown ann
-        Annotated ann res = f a
+        Annotated cks r = f a
 
 
 splitAnd
@@ -105,15 +140,15 @@ splitAnd
 splitAnd st spawn (Pred lblB fB) (Pred lblC fC) = Pred lbls f
   where
     lbls = Tree st (Two lblB lblC)
-    f a = Tree out children
+    f a = Out cks res
       where
-        out = Output res shown dynL
-        Annotated dynL (b, c) = spawn a
-        outB@(Tree (Output resB _ _) _) = fB b
-        outC@(Tree (Output resC _ _) _) = fC c
-        (children, res)
-          | Prelude.not resB = (One outB, False)
-          | otherwise = (Two outB outC, resB && resC)
+        Annotated cks (b, c) = spawn a
+        outB@(Out _ cB) = fB b
+        outC@(Out _ cC) = fC c
+        (resB, resC) = (outResult cB, outResult cC)
+        res
+          | Prelude.not resB = Child1 False showChildren outB
+          | otherwise = Child2 (resB && resC) showChildren outB outC
 
 
 splitOr
@@ -126,15 +161,15 @@ splitOr
 splitOr st spawn (Pred lblB fB) (Pred lblC fC) = Pred lbls f
   where
     lbls = Tree st (Two lblB lblC)
-    f a = Tree out children
+    f a = Out cks res
       where
-        out = Output res shown dynL
-        Annotated dynL (b, c) = spawn a
-        outB@(Tree (Output resB _ _) _) = fB b
-        outC@(Tree (Output resC _ _) _) = fC c
-        (children, res)
-          | resB = (One outB, True)
-          | otherwise = (Two outB outC, resB || resC)
+        Annotated cks (b, c) = spawn a
+        outB@(Out _ cB) = fB b
+        outC@(Out _ cC) = fC c
+        (resB, resC) = (outResult cB, outResult cC)
+        res
+          | resB = Child1 True showChildren outB
+          | otherwise = Child2 (resB || resC) showChildren outB outC
 
 
 wrap
@@ -145,27 +180,23 @@ wrap
 wrap st spawn (Pred lbl f) = Pred lbl' f'
   where
     lbl' = Tree st (One lbl)
-    f' a = Tree out (One chld)
+    f' a = Out ann res
       where
-        Annotated dynL b = spawn a
-        chld@(Tree (Output chldRes _ _) _) = f b
-        out = Output chldRes shown dynL
-
-
+        Annotated ann b = spawn a
+        res = Hollow (f b)
 switch
   :: [Chunk]
   -> (a -> Annotated (Either b c))
   -> Pred b
   -> Pred c
   -> Pred a
-switch st split (Pred lblB fB) (Pred lblC fC) = Pred lbls f
+switch st split (Pred lblB fB) (Pred lblC fC) = Pred lbl' f
   where
-    lbls = Tree st (Two lblB lblC)
-    f a = Tree out (One child)
+    lbl' = Tree st (Two lblB lblC)
+    f a = Out ann (Hollow child)
       where
-        Annotated dynL ei = split a
-        out = Output res shown dynL
-        child@(Tree (Output res _ _) _) = case ei of
+        Annotated ann ei = split a
+        child = case ei of
           Left b -> fB b
           Right c -> fC c
 
@@ -179,14 +210,14 @@ and
 and st fDyn (Pred lblA fA) (Pred lblB fB) = Pred lbls f
   where
     lbls = Tree st (Two lblA lblB)
-    f a = Tree out children
+    f a = Out dyn c
       where
-        out = Output res shown (fDyn a)
-        outA@(Tree (Output resA _ _) _) = fA a
-        outB@(Tree (Output resB _ _) _) = fB a
-        (children, res)
-          | Prelude.not resA = (One outA, False)
-          | otherwise = (Two outA outB, resA && resB)
+        dyn = fDyn a
+        outA@(Out _ oA) = fA a
+        outB@(Out _ oB) = fB a
+        (resA, resB) = (outResult oA, outResult oB)
+        c | Prelude.not resA = Child1 False showChildren outA
+          | otherwise = Child2 (resA && resB) showChildren outA outB
 
 
 or
@@ -198,14 +229,29 @@ or
 or st fDyn (Pred lblA fA) (Pred lblB fB) = Pred lbls f
   where
     lbls = Tree st (Two lblA lblB)
-    f a = Tree out children
+    f a = Out dyn c
       where
-        out = Output res shown (fDyn a)
-        outA@(Tree (Output resA _ _) _) = fA a
-        outB@(Tree (Output resB _ _) _) = fB a
-        (children, res)
-          | resA = (One outA, True)
-          | otherwise = (Two outA outB, resA || resB)
+        dyn = fDyn a
+        outA@(Out _ oA) = fA a
+        outB@(Out _ oB) = fB a
+        (resA, resB) = (outResult oA, outResult oB)
+        c | resA = Child1 True showChildren outA
+          | otherwise = Child2 (resA || resB) showChildren outA outB
+
+not
+  :: [Chunk]
+  -> (a -> [Chunk])
+  -> Pred a
+  -> Pred a
+not st fDyn (Pred lbl f) = Pred lbl' f'
+  where
+    lbl' = Tree st (One lbl)
+    f' a = Out (fDyn a) (Child1 res showChildren child)
+      where
+        child@(Out _ c) = f a
+        res = Prelude.not . outResult $ c
+
+{-
 
 
 not
@@ -221,3 +267,4 @@ not st fDyn (Pred lbl f) = Pred lbl' f'
         chld@(Tree (Output chldRes _ _) _) = f a
         out = Output (Prelude.not chldRes) shown (fDyn a)
 
+-}
